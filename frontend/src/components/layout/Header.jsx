1503d7e8b70e6228fb9af161ceddd678859d1866
mariyaLyapova <Mariya.Lyapova@bosch.com>
2026-02-26 15:26:38 +0200

    Update UI: add user avatar, reduce padding and spacing across pages

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -25,7 +25,23 @@ export default function Header() {
                 Create Route
               </Link>
               <Link to="/profile" className="user-link">
-                {user.username}
+                {user.avatar ? (
+                  <img 
+                    src={user.avatar} 
+                    alt={user.username} 
+                    className="user-avatar"
+                  />
+                ) : (
+                  <svg 
+                    className="user-icon" 
+                    fill="currentColor" 
+                    viewBox="0 0 20 20" 
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+                  </svg>
+                )}
+                <span className="user-name">{user.username}</span>
               </Link>
               <button onClick={logout} className="btn-logout">
                 Logout
